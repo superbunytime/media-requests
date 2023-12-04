5b60e003458f805201d046e271ff117431726dfe
Eric White <led.ecodyswhite@gmail.com>
2023-12-04 15:43:05 -0800

    foaming at the mouth

--- main.js
+++ main.js
@@ -30,8 +30,8 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-//   event.target.playVideo();
-//commenting that out to see if it stops autoplay and breaks nothing
+  //   event.target.playVideo();
+  //commenting that out to see if it stops autoplay and breaks nothing
 }
 
 // 5. The API calls this function when the player's state changes.
@@ -39,11 +39,11 @@ function onPlayerReady(event) {
 //    the player should play for six seconds and then stop.
 var done = false;
 function onPlayerStateChange(event) {
-//   if (event.data == YT.PlayerState.PLAYING && !done) {
-//     setTimeout(stopVideo, 6000);
-//     done = true;
-//   }
-//commented out the code that stops the video playing after 6 seconds
+  //   if (event.data == YT.PlayerState.PLAYING && !done) {
+  //     setTimeout(stopVideo, 6000);
+  //     done = true;
+  //   }
+  //commented out the code that stops the video playing after 6 seconds
 }
 function stopVideo() {
   player.stopVideo();
@@ -56,17 +56,17 @@ const tBody = document.getElementsByTagName("tbody");
 const button = document.getElementById("songButton");
 const songText = document.getElementById("songText");
 //declaring these with let because they'll need to be dynamically updated
-let youtubeURL = document.getElementsByTagName("td")[0].innerText
-let youtubeURLSlice = youtubeURL.slice(youtubeURL.lastIndexOf("/") + 1)
-let nowPlaying = document.getElementById("nowPlaying")
+let youtubeURL = document.getElementsByTagName("td")[0].innerText;
+let youtubeURLSlice = youtubeURL.slice(youtubeURL.lastIndexOf("/") + 1);
+let nowPlaying = document.getElementById("nowPlaying");
 
 class Song {
-    constructor(url, site, source) {
-      this.url = url;
-      this.site = site;
-      this.source = source;
-    }
+  constructor(url, site, source) {
+    this.url = url;
+    this.site = site;
+    this.source = source;
   }
+}
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
@@ -77,9 +77,8 @@ function newSong(url, site = "youtube", source = "puppypotion") {
   return new Song(url, site, source);
 }
 
-
-function generateSongMarkup(song) {
-  let row = table.insertRow(-1);
+function generateSongMarkup(song, index = -1) {
+  let row = table.insertRow(index);
 
   let cell1 = row.insertCell(0);
   let cell2 = row.insertCell(1);
@@ -90,45 +89,54 @@ function generateSongMarkup(song) {
   cell3.innerHTML = song.source;
 }
 
-
 //alright now add a button to submit new songs.
 
 function formHandler(event) {
-
   generateSongMarkup(newSong(songText.value));
 }
 
 //moving on to next steps
 //it may be wise to do data sanitization (typecheck if the link is a valid link (youtube, etc))
 
-
 //alright now get the first url in the table
 
-function popSong(){
-    //get the most recent song in the queue, pit it in the player, now playing div
-    let requester = document.getElementsByTagName("td")[2].innerText
-    youtubeURL = document.getElementsByTagName("td")[0].innerText
-    let originPlatform = document.getElementsByTagName("td")[1].innerText
-    youtubeURLSlice = youtubeURL.slice(youtubeURL.lastIndexOf("/") + 1)
-    nowPlaying.innerHTML = `The current Track is <a href="${youtubeURL}">${youtubeURL}</a>, requested by ${requester} platform: ${originPlatform}`
-    //this will look funky if it's from backup playlist
-    table.deleteRow(1);
-    //this creates a data peristency problem for later, unless you preserve the platform data.
-    //that sounds like a future problem for future me.
-    return new Song(youtubeURL, originPlatform, requester)
+function popSong() {
+  //get the most recent song in the queue, pit it in the player, now playing div
+  let requester = document.getElementsByTagName("td")[2].innerText;
+  let originPlatform = document.getElementsByTagName("td")[1].innerText;
+  nowPlaying.innerHTML = `The current Track is <a href="${youtubeURL}">${youtubeURL}</a>, requested by ${requester} platform: ${originPlatform}`;
+  //this will look funky if it's from backup playlist
+  table.deleteRow(1);
+  //this creates a data peristency problem for later, unless you preserve the platform data.
+  //that sounds like a future problem for future me.
+  return new Song(youtubeURL, originPlatform, requester);
 }
 
-function unpopSong(poppedSong){
-    //alright now if someone wants to switch playlists, how would one do that?
-    //that's something to consider, but beyond the scope of this function
-    //this function is only concerned with unpopping the song
-    //which is something that needs to happen before shifting playlists
-    //so just re-add the song at index[1]
-    //but that problem for future me turned into a problem for present me
-    //if i use popsong to return a song object, i can just take that as the argument!!
-    //hey that worked!
-    //i'm very smart.
+function unpopSong(song) {
+  generateSongMarkup(poppedSong, 1);
+  //need to remove the popped song from the now playing section too.
+  nowPlaying.innerHTML = "context menu :D";
+  //need to write a function to update the youtube player and call it in the unpop song function
 }
 
-let poppedSong = popSong()
-console.log(poppedSong)
+function updatePlayer(youtubePlayer) {
+  console.log(player.h.g.videoId);
+  // player.clearVideo();
+  //player.clearVideo() doesn't work and the documentation for it is actually impossible to find
+  //however, you can set the songId to an empty string to achieve the desired effect.
+  //as an aside, you can use the player object to get the title, for the contextual data on the player display
+  //probably thumbnail too.
+
+  //why is it returning the youtube player, but calling it "Or"
+  //that does not make any sense and i think i have brain damage now
+  //I SEE YOU. console.log(player.h.g.videoId);
+}
+
+let poppedSong = popSong();
+//this needs to be at the bottom, or at least not at the top, due to scope error i haven't figured out yet
+
+const myTimeout = setTimeout(() => {
+  updatePlayer(player);
+}, 3000);
+
+// const myTimeout = setTimeout( () => {unpopSong((poppedSong))}, 5000);
